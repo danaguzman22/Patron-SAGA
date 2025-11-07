6360e64fc9ae096247d7a66d26f5a2afd4af2a33
Sebastián Aguilera <sebaalejandro.aguilera@gmail.com>
2025-11-07 19:37:15 -0300

    microservicio inventario

---
+++ ms-inventario/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Response, status
+import random
+
+app = FastAPI()
+
+@app.get("/inventario")
+def verificar_inventario():
+  stock = random.choice([True, False])
+
+  if stock:
+        return Response(
+            content='{"mensaje": "Hay stock disponible"}',
+            media_type="application/json",
+            status_code=status.HTTP_200_OK
+        )
+  else:
+      return Response(
+          content='{"mensaje": "Sin stock"}',
+          media_type="application/json",
+          status_code=status.HTTP_409_CONFLICT
+      )
